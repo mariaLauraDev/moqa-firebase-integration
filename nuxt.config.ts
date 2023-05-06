43f114ea5ceb81f6d1ace160b36adecf6c638ff4
@laura.alves <laura.alves@gocase.com.br>
2023-05-06 16:23:28 -0300

    feat: add auth firebase service

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,14 @@ export default defineNuxtConfig({
     public : { 
       email: process.env.EMAIL,
       password: process.env.PASSWORD,
+      firebaseConfig: {
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID,
+        appId: process.env.APP_ID
+      }
     },
     private: {
 
